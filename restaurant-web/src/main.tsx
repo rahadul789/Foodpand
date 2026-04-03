c13c8564adf44ec6851469b1063842c52a862fe4
Reason <reason@gmail.com>
2026-04-03 08:46:57 +0600

    Before adding admin web both app push noti

--- restaurant-web/src/main.tsx
+++ restaurant-web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-
+// okay
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./lib/auth";
